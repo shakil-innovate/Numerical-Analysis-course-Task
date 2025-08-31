477fed0e43f338c7896f2531d28e282572e04b19
shakil-innovate <shakil.innovate@gmail.com>
2025-08-31 09:58:35 +0600

    Update Bisection_Method.cpp

--- Bisection_Method.cpp
+++ Bisection_Method.cpp
@@ -1,78 +1,46 @@
-//--------------------------------------//
+//*********************************************//
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+
 typedef double db;
 #define nl '\n'
-const ll mod=1e9+7;
-const ll INF=1e9;
-#define vi vector<ll>
-#define vii vector<vi>
+//********************************************************//
 
-ll add(ll a,ll b){ return ((a%mod + b%mod)+ mod ) % mod;}
-ll sub(ll a,ll b){ return ((a%mod - b%mod)+mod)% mod;}
-ll mul(ll a,ll b){ return ((a%mod) * (b%mod))%mod ;}
-ll power(ll a, ll b){
-    ll res=1;
-    a%=mod;
-        while(b > 0){
-            if(b & 1)res=(res*a) %mod;
-            a=(a*a)%mod;
-            b=b>>1;
-        }
-    return res;
-}
-ll inv(ll a){return power(a,mod-2);}
-//---------------------------------------//
-db tolerance=.000001;
+const db tolerance=1e-6;
 
-db calculateFunction(db x)
+db func(db x)
 {
-    return 4*x*x+3*x-3;
+   return 4*x*x+3*x-3;
 }
 
 void bisection(db a,db b)
 {
-    db error=1e5;
-    ll step=1;
-    db mid=10;
+   db mid=(a+b)/2;
+   db error=fabs(a-b);
 
-    while(error>tolerance)
-    {
-         mid=(a+b)/2;
-        error=abs(a-b);
+   ll step=0;
+   cout<<"Step: "<<"a       "<<"b     "<<"error"<<nl;
+   while(error>tolerance)
+   {
+      mid=(a+b)/2;
+      if(func(a)*func(mid)<=0)
+      {
+         b=mid;
+      }
+      else a=mid;
+      error=fabs(a-b);
+      step++;
+   cout<<step<<"     "<<a<<"      "<<b<<"  "<<error<<nl;
 
-        db res=calculateFunction(a)*calculateFunction(mid);
-        if(res<=0)
-        {
-            b=mid;
-        }
-        else a=mid;
-
-        cout<<"step :"<<step<<" "<<"approximation value: "<<mid<<" Error:"<<error<<nl;
-        step++;
-    }
-
-    cout<<"Final Result:"<<mid<<nl;
-    
+   }
+   cout<<"Root is"<<mid<<nl;
 }
 
-void shakil()
-{
-    cout<<"Enter Guess value:";
-    db a,b; cin>>a>>b;
-    bisection(a,b);
-}
 int  main()
 {
-   ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    ll tt=1;
-    // cin>>tt;
-  
-    for(int i=1;i<=tt;i++)
-    {
-      shakil();
-    }
+   cout<<func(.75)<<" "<<func(1)<<nl;
+   cout<<"Enter guessing two val:"<<nl;
+   db a,b;  cin>>a>>b;
+   bisection(a,b);
 }
-
