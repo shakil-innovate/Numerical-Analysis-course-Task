f2dfa37f77dd37ec356a4c0ed24c54bc58eb143b
shakil-innovate <shakil.innovate@gmail.com>
2025-08-31 10:23:28 +0600

    correctness

--- Falsi_method.cpp
+++ Falsi_method.cpp
@@ -1,78 +1,55 @@
-//--------------------------------------//
+//*********************************************//
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+
 typedef double db;
 #define nl '\n'
-const ll mod=1e9+7;
 const ll INF=1e9;
-#define vi vector<ll>
-#define vii vector<vi>
+//********************************************************//
 
-ll add(ll a,ll b){ return ((a%mod + b%mod)+ mod ) % mod;}
-ll sub(ll a,ll b){ return ((a%mod - b%mod)+mod)% mod;}
-ll mul(ll a,ll b){ return ((a%mod) * (b%mod))%mod ;}
-ll power(ll a, ll b){
-    ll res=1;
-    a%=mod;
-        while(b > 0){
-            if(b & 1)res=(res*a) %mod;
-            a=(a*a)%mod;
-            b=b>>1;
-        }
-    return res;
-}
-ll inv(ll a){return power(a,mod-2);}
-//---------------------------------------//
-db tolerance=.000001;
+const db tolerance=1e-10;
 
-db calculateFunction(db x)
+db f(db x)
 {
-    return 4*x*x+3*x-3;
+   return (1.0*log(3*x)-3);
 }
 
-void Falsi(db a,db b)
+db res(db a,db b)
 {
-    db error=1e5;
-    ll step=1;
-    db mid=10;
-
-    while(error>tolerance)
-    {
-         mid=(a*calculateFunction(b)-b*calculateFunction(a))/(calculateFunction(b)-calculateFunction(a));
-        error=abs(a-b);
-
-        db res=calculateFunction(a)*calculateFunction(mid);
-        if(res<=0)
-        {
-            b=mid;
-        }
-        else a=mid;
-
-        cout<<"step :"<<step<<" "<<"approximation value: "<<mid<<" Error:"<<error<<nl;
-        step++;
-    }
-
-    cout<<"Final Result:"<<mid<<nl;
-    
+   return (b*f(a)-a*f(b))/(f(a)-f(b))*1.0;
 }
 
-void shakil()
+void regular_falsi(db a,db b)
 {
-    cout<<"Enter Guess value:";
-    db a,b; cin>>a>>b;
-    Falsi(a,b);
+  db prev_c=INF*1.0;
+  db c=res(a,b);
+
+   ll step=0;
+   cout<<"Step: "<<"a       "<<"b     c      "<<"error"<<nl;
+   db error=INF*1.0;
+
+   while(error>tolerance)
+   {
+      c=res(a,b);
+      if(f(a)*f(c)<=0)
+      {
+         b=c;
+      }
+      else a=c;
+      error=fabs(prev_c-c);
+      prev_c=c;
+      step++;
+      cout<<step<<"   "<<a<<"     "<<b<<"    "<<c<<"     "<<error<<nl;
+      if(fabs(f(c))<=tolerance)break;
+      
+   }
+   cout<<"Root is: "<<prev_c<<nl;
 }
+
 int  main()
 {
-   ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    ll tt=1;
-    // cin>>tt;
-  
-    for(int i=1;i<=tt;i++)
-    {
-      shakil();
-    }
+   cout<<"Enter guessing two val:"<<nl;
+   db a,b;  cin>>a>>b;
+   regular_falsi(a,b);
 }
-
